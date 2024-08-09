418867c888ece983c43e422d3be70387c3310b8b
Roy Zalta <roey.zalta@gmail.com>
2024-08-09 15:32:01 +0300

    fix gitignore to add data folder and files

---
+++ src/data/transform.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from data_preparation import basic_preparation
+from feature_engineering import map_categorical, create_features
+
+def transform_df(df):
+    good_columns = [
+        'customerID', 'gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure', 'PhoneService',
+        'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
+        'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
+        'PaymentMethod', 'MonthlyCharges', 'TotalCharges'
+    ]
+    df = basic_preparation(df, good_columns)
+    df = map_categorical(df)
+    df = create_features(df)
+    return df
