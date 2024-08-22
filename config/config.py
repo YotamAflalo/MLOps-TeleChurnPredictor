62bc8832ef8f5c6de5df75f0e22afd1fce7daace
yotam <yotam.aflalo433@gmail.com>
2024-08-22 15:20:56 +0300

    connecting the app to db and writing logs

--- config/config.py
+++ config/config.py
@@ -3,3 +3,4 @@ INPUT_PATH_BATCH = ''
 OUTPUT_PATH_BATCH = ''
 REQUIRED_COLUMNS = ['Contract', 'tenure']
 OPTIONAL_COLUMNS = ['TotalCharges', 'PhoneService']
+DATABASE_URL = "postgresql://user:password@db:5432/api_logs"
