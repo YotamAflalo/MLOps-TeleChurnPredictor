86844067819102866bc0a0ad77890a117f9b1e8d
Roey Zalta <roey.zalta@gmail.com>
2024-08-26 12:24:16 +0300

    added modularity to beam_processing
    - change input files to be deleted after preprocess
    - made sure beam can process all types of files
    - created dedicated paths for input and output

--- src/models/batch/beam_processing.py
+++ src/models/batch/beam_processing.py
@@ -3,16 +3,17 @@ from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
 import pandas as pd
 import pickle
 import datetime
-# import sys
-# from pathlib import Path
 import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
 import csv
+# from config.config import INPUT_DIR,MODEL_PATH,OUTPUT_DIR
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Define paths relative to the current script
-DATA_PATH = os.path.join(current_dir, '..', '..', '..', 'data', 'raw', 'database_input.csv')
+INPUT_DIR = os.path.join(current_dir, '..', '..', '..', 'data', 'batch_input')
 MODEL_PATH = os.path.join(current_dir, '..', '..', '..', 'models', 'churn_model.pickle')
-OUTPUT_DIR = os.path.join(current_dir, 'output')
+OUTPUT_DIR = os.path.join(current_dir, '..', '..', '..', 'data', 'batch_results')
+
 
 # Define a custom transform to clean and preprocess data
 
@@ -20,13 +21,25 @@ def discard_incomplete(data):
     """Filters out records that don't have an information about Contract type."""
     return len(data['Contract']) > 0 and len(data['tenure']) > 0
 
+
 class discard_incomplete_dofn(beam.DoFn):
-        def process(self, element):
-            dataset = pd.DataFrame([element])
-            dataset.reset_index(inplace=True)
-            #print(dataset.columns)
-            dataset.dropna(axis=1,subset=['Contract','tenure'])
-            return [dataset.to_dict('records')[0]]     
+    def process(self, element):
+        dataset = pd.DataFrame([element])
+        dataset.reset_index(inplace=True)
+        #print(dataset.columns)
+        dataset.dropna(axis=1, subset=['Contract', 'tenure'])
+        return [dataset.to_dict('records')[0]]
+
+# validate and transform to ensure the data is in the correct format
+def validate_and_transform(value, expected_type, default_value):
+    if value in [' ', '', None]:
+        return default_value
+    try:
+        return expected_type(value)
+    except (ValueError, TypeError):
+        print(f"Warning: Invalid value {value}. Using default value {default_value}")
+        return default_value
+
 
 class TransformData(beam.DoFn):
     def process(self, element):
@@ -34,37 +47,40 @@ class TransformData(beam.DoFn):
         
         # Fill nulls and clean data
         if 'TotalCharges' in dataset.columns:
-            dataset['TotalCharges'] = dataset['TotalCharges'].fillna(2279)
-            dataset['TotalCharges'] = dataset['TotalCharges'].replace(' ', '2279')
-            dataset['TotalCharges'] = dataset['TotalCharges'].astype(float)
+            dataset['TotalCharges'] = dataset['TotalCharges'].apply(
+                    lambda x: validate_and_transform(x, float, 2279.0)
+            )
         else:
             dataset['TotalCharges'] = 2279.0
-
+        
         if 'PhoneService' in dataset.columns:
             dataset['PhoneService'] = dataset['PhoneService'].fillna('No')
-            dataset['PhoneService'] = dataset['PhoneService'].map({'Yes': 1, 'No': 0})
+            dataset['PhoneService'] = dataset['PhoneService'].map(lambda x: 1 if x == 'Yes' else 0)
         else:
             dataset['PhoneService'] = 0
-
+        
         # One-hot encoding for Contract column
         dataset = dataset.join(pd.get_dummies(dataset['Contract']).astype(int))
-
+        
         # Ensure all one-hot encoded columns are present
         for val in ['Month-to-month', 'One year', 'Two year']:
             if val not in dataset.columns:
                 dataset[val] = 0
-                
+        
         return [dataset.to_dict('records')[0]]
+
+
 def get_csv_headers(file_path):
     with open(file_path, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
         headers = next(csv_reader)
     return headers
 
+
 class Predict(beam.DoFn):
     def __init__(self, model):
         self.model = model
-        
+    
     def process(self, element):
         dataset = pd.DataFrame([element])
         result_columns = ['TotalCharges', 'Month-to-month', 'One year', 'Two year', 'PhoneService', 'tenure']
@@ -72,35 +88,52 @@ class Predict(beam.DoFn):
         element['prediction'] = prediction[0]
         return [element]
 
+
 def parse_csv_line(line, headers):
     values = line.split(',')
     return dict(zip(headers, values))
+
+
 def print_debug_info(element):
     print(f"Debug Info: {element}")
     return [element]
+
+
 def run():
     # Load the pre-trained model
     with open(MODEL_PATH, 'rb') as f:
         model = pickle.load(f)
-    time =  str(datetime.datetime.now()).replace(' ','_').replace(':','_')
-    options = PipelineOptions()
-
-    p = beam.Pipeline(options=options)
-    headers = get_csv_headers(DATA_PATH)
-    data = (
-        p 
-        | 'ReadData' >> beam.io.ReadFromText(DATA_PATH, skip_header_lines=1)
-        | 'SplitData' >> beam.Map(lambda x: x.split(','))
-        | 'FormatToDict' >> beam.Map(lambda x: dict(zip(headers, x)))
-        # | 'FormatToDict' >> beam.Map(lambda x: {"customerID": x[1], "tenure": x[6],
-        #                                          "PhoneService": x[7], "Contract": x[16], "TotalCharges": x[20]})
-        | 'DeleteIncompleteData' >> beam.Filter(discard_incomplete)
-        | 'TransformData' >> beam.ParDo(TransformData())
-        | 'Predict' >> beam.ParDo(Predict(model))
-        | 'WriteResults' >> beam.io.WriteToText(os.path.join(OUTPUT_DIR, f'results_{time}.csv'))
-    )
-
-    p.run().wait_until_finish()
-
+    for file in os.listdir(INPUT_DIR):
+        if file.endswith(".csv"):
+            DATA_PATH = os.path.join(INPUT_DIR, file)
+            print(f"Processing file: {DATA_PATH}")
+            # log time of prediction
+            time = datetime.datetime.now().strftime('%d_%H_%M')
+            # log file name
+            # here will be the y-log log of the files name and time
+            options = PipelineOptions()
+            # define the pipeline options
+            p = beam.Pipeline(options=options)
+            # define the pipeline headers from the csv file
+            headers = get_csv_headers(DATA_PATH)
+            data = (
+                    p
+                    | 'ReadData' >> beam.io.ReadFromText(DATA_PATH, skip_header_lines=1)
+                    | 'SplitData' >> beam.Map(lambda x: x.split(','))
+                    | 'FormatToDict' >> beam.Map(lambda x: dict(zip(headers, x)))
+                    | 'DeleteIncompleteData' >> beam.Filter(discard_incomplete)
+                    | 'TransformData' >> beam.ParDo(TransformData())
+                    | 'Predict' >> beam.ParDo(Predict(model))
+                    | 'WriteResults' >> beam.io.WriteToText(os.path.join(OUTPUT_DIR, f'results_{file}_{time}.csv'))
+            )
+            
+            
+            p.run().wait_until_finish()
+            print(f"Processing of {DATA_PATH} finished.")
+            # delete the file after from batch_input
+            os.remove(DATA_PATH)
+            print(f"File {DATA_PATH} deleted.")
+            
+            
 if __name__ == '__main__':
     run()
